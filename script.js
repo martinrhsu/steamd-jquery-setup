b9500b7f341884cc525776e4a3b861d348bbd618
martinrhsu <martin.r.hsu@gmail.com>
2015-07-30 10:27:29 -0400

    GitHub Pull

---
+++ script.js
@@ -0,0 +1,15 @@
+$('#alert-button').click(function(){
+	alert('Hello world!');
+});
+
+$('#danger-button').click(function(){
+	alert('The site you are trying to visit has been marked by Google Adware as a potential malware site');
+});
+
+$('#hide-button').click(function(){
+	$('#hide-p').hide();
+});
+
+$('#show-button').click(function(){
+	$('#hide-p').show();
+});
